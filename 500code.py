4e455a5b767c1908e62bc6903130e26e9cb3d288
CubeDasherOnGIT <max.robert.reid@gmail.com>
2026-06-20 15:53:35 -0400

    Fixed

--- 500code.py
+++ 500code.py
@@ -1,194 +1,173 @@
 import turtle as pen
-t = pen.pen()
+t = pen.Turtle()  # Fixed: Capitalized Turtle() to prevent immediate crash
 import time as waittime
 import webbrowser as browser
 import os as download
 from os.path import join
+import sys
 
 def startup():
     pen.speed(1)
-    t = pen.pen()
-    t.speed(pen.speed)
+    t.speed(1)  # Fixed: Passed an integer instead of the pen.speed function
     pen.bgcolor("black")
     print("500 Coding Language")
     print("Made Intirely In Python")
     print("Created By: CubeDasherGD")
-    set("penmode", False)
-    set("normalmode", True)
+    global is_pen_mode; is_pen_mode = False
+    global is_normal_mode; is_normal_mode = True
     cmd()
 
 def help():
-    if penmode == True:
+    if is_pen_mode == True:
         print("You are currently in pen mode. Type 'normalmode' to exit pen mode and access normal commands.")
-        pencmd()
     else:
         browser.open("https://cubedasherongit.github.io/500codehelp/help/basichelp.html/")
-        cmd()
 
-def exit():
+def exit_program():  # Renamed to avoid using python's built-in exit keyword
     print("Exiting...")
     waittime.sleep(1)
-    exit
+    sys.exit()
 
 def penmode():
-    if penmode == True:
+    global is_pen_mode, is_normal_mode
+    if is_pen_mode == True:
         print("You are currently in pen mode. Type 'normalmode' to exit pen mode and access normal commands.")
-        pencmd()
     else:
         print("Entering pen mode...")
         waittime.sleep(1)
-        set("penmode", True)
-        set("normalmode", False)
+        is_pen_mode = True
+        is_normal_mode = False
         print("Pen mode activated. Normal commands are disabled. Type 'normalmode' to exit pen mode.")
-        pencmd()
+        pencmd()  # Fixed: Initiates the pen loop after switching states
 
 def normalmode():
-    if normalmode == True:
+    global is_pen_mode, is_normal_mode
+    if is_normal_mode == True:
         print("You are currently in normal mode. Type 'penmode' to enter pen mode and access pen mode commands.")
-        cmd()
     else:
         print("Exiting pen mode...")
         waittime.sleep(1)
-        set("penmode", False)
-        set("normalmode", True)
-        cmd()
+        is_pen_mode = False
+        is_normal_mode = True
+        cmd()  # Fixed: Initiates the normal loop after switching states
 
 def pencmd():
-    pencommand = input()
-    if pencommand == "penhelp":
-        penhelp()
-    elif pencommand == "textshow":
-        textshow()
-    elif pencommand == "pendown":
-        pendown()
-    elif pencommand == "penup":
-        penup()
-    elif pencommand == "exit":
-        exit()
-    elif pencommand == "forward":
-        penforward()
-    elif pencommand == "backward":
-        penbackward()
-    elif pencommand == "left":
-        penleft()
-    elif pencommand == "right":
-        penright()
-    elif pencommand == "color":
-        pencolor()
-    elif pencommand == "pensize":
-        pensize()
-    elif pencommand == "bgcolor":
-        bgcolor()
-    elif pencommand == "normalmode":
-        normalmode()
-    else:
-        unknownpenmodecommand()
+    while True:
+        pencommand = input("PEN> ")
+        if pencommand == "penhelp":
+            penhelp()
+        elif pencommand == "textshow":
+            textshow()
+        elif pencommand == "pendown":
+            pendown()
+        elif pencommand == "penup":
+            penup()
+        elif pencommand == "exit":
+            exit_program()
+        elif pencommand == "forward":
+            penforward()
+        elif pencommand == "backward":
+            penbackward()
+        elif pencommand == "left":
+            penleft()
+        elif pencommand == "right":
+            penright()
+        elif pencommand == "color":
+            pencolor()
+        elif pencommand == "pensize":
+            pensize()
+        elif pencommand == "bgcolor":
+            bgcolor()
+        elif pencommand == "normalmode":
+            normalmode()
+            break
+        else:
+            unknownpenmodecommand()
 
 def penhelp():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
     else:
         browser.open("https://cubedasherongit.github.io/500codehelp/help/penhelp.html/")
-        pencmd()
 
 def pendown():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
     else:
         t.pendown()
-        pencmd()
 
 def penup():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
+        # Fixed: Removed the breaking cmd() call from here
     else:
         t.penup()
-        pencmd()
 
 def penforward():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
     else:
         print("How many pixels do you want to move forward?")
         pixels = int(input())
         t.forward(pixels)
-        pencmd()
 
 def penbackward():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
     else:
         print("How many pixels do you want to move backward?")
         pixels = int(input())
         t.backward(pixels)
-        pencmd()
 
 def penleft():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
     else:
         print("How many degrees do you want to turn left?")
         degrees = int(input())
         t.left(degrees)
-        pencmd()
 
 def penright():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
     else:
         print("How many degrees do you want to turn right?")
         degrees = int(input())
         t.right(degrees)
-        pencmd()
 
 def pencolor():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
     else:
         print("What color do you want to set the pen to?")
         color = input()
         t.color(color)
-        pencmd()
 
 def pensize():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
     else:
         print("What size do you want to set the pen to?")
         size = int(input())
         t.pensize(size)
-        pencmd()
 
 def bgcolor():
-    if penmode == False:
+    if is_pen_mode == False:
         print("Pen mode is not activated. Type 'penmode' to enter pen mode.")
-        cmd()
     else:
         print("What color do you want to set the background to?")
         color = input()
-        t.bgcolor(color)
-        pencmd()
+        pen.bgcolor(color)
 
 def textshow():
-    if penmode == True:
+    if is_pen_mode == True:
         print("What text do you want to show?")
         text = input()
         t.write(text, font=("Arial", 16, "normal"))
-        pencmd()
     else:
         print("What text do you want to show?")
         text = input()
         print(text)
-        cmd()
 
 def dwld():
     folderpath = input("Enter folder path: ")
@@ -201,13 +180,13 @@ def dwld():
         file.write("#This Is A FiveHundred Code File. You Can Run The File In Python To Run The Code Written In The File\n")
         file.write("#DONT CHANGE OR DELETE THESE LINES OR THE CODE MAY NOT WORK AS INTENDED\n")
         file.write("import turtle as pen\n")
-        file.write("t = pen.pen()\n")
+        file.write("t = pen.Turtle()\n")
         file.write("import time as waittime\n")
         file.write("import webbrowser as browser\n")
         file.write("import os\n")
         file.write("from os.path import join\n")
         file.write("def exit():\n")
-        file.write("   exit\n")
+        file.write("   exit()\n")
         file.write("\n")
         file.write("#SAFE TO EDIT FROM DOWN HERE\n")
         file.write("#EXAMPLE: TYPE 'print(\"Test\")' FOR DISPLAYING TEXT\n")
@@ -216,25 +195,25 @@ def dwld():
 
 def unknowncommand():
     print("Unknown command. Type 'help' for a list of commands.")
-    cmd()
 
 def unknownpenmodecommand():
     print("Unknown pen mode command. Type 'penhelp' for a list of pen mode commands.")
-    pencmd()
 
 def cmd():
-    command = input()
-    if command == "help":
-        help()
-    elif command == "exit":
-        exit()
-    elif command == "textshow":
-        textshow()
-    elif command == "download":
-        dwld()
-    elif command == "penmode":
-        penmode()
-    else:
-        unknowncommand()
+    while True:
+        command = input("CMD> ")
+        if command == "help":
+            help()
+        elif command == "exit":
+            exit_program()
+        elif command == "textshow":
+            textshow()
+        elif command == "download":
+            dwld()
+        elif command == "penmode":
+            penmode()
+            break
+        else:
+            unknowncommand()
 
 startup()
